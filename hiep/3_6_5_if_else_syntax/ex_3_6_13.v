fdd0f598b1661d845ef799e49d1f9d3d5e536d73
guwoh <hoanghiep2k5@gmail.com>
2025-08-14 12:55:16 +0700

    ex 3.6.13

---
+++ hiep/3_6_5_if_else_syntax/ex_3_6_13.v
@@ -0,0 +1,14 @@
+module ex_3_6_13 #( parameter n = 8) (
+    input [n-1:0] a, b,
+    output reg [n-1:0] y,
+    input [1:0] op
+);
+    always @(a or b or op) begin 
+    if(op == 0)
+        y = a + b;
+    else if(op == 1)
+        y = a & b;
+    else 
+        y = 8'h00; // default case
+    end 
+endmodule 
